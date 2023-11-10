2c9000545e3c6f5a634c6c7447ac29561dc80ffb
martinmulama <120002279+martinmulama@users.noreply.github.com>
2023-11-10 13:06:47 +0300

    pre deployment updates

--- app.py
+++ app.py
@@ -255,3 +255,10 @@ def cancel_booking(booking_id):
         return jsonify(message='Booking canceled successfully'), 200
     else:
         return jsonify(message='Booking not found'), 404
+    
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
